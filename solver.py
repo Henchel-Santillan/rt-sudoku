858b6e7f37231bb488a55f334160997327801ff6
The Level Three Chin <henchelsantillan@gmail.com>
2021-05-06 10:33:30 -0400

    removed redundant tokens from solver; fixed style according to PEP

--- solver.py
+++ solver.py
@@ -2,25 +2,25 @@ def empty(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if board[row][col] == 0:
-                return (row, col)
+                return row, col
 
     return None
 
 
 def valid(board, val, pos):
-    size = len(board);
+    size = len(board)
 
-    #row check
+    # row check
     for col in range(size):
         if col != pos[1] and board[pos[0]][col] == val:
             return False
 
-    #column check
+    # column check
     for row in range(size):
         if row != pos[0] and board[row][pos[1]] == val:
             return False
 
-    #box check: get upper vertex of 3x3 submatrix
+    # box check: get upper vertex of 3x3 submatrix
     uv_x, uv_y = (pos[0] / 3) * 3, (pos[1] / 3) * 3
 
     for row in range(uv_x, uv_x + 3):
@@ -32,12 +32,12 @@ def valid(board, val, pos):
 
 
 def solve(board):
-    empty = empty(board)
+    find_empty = empty(board)
 
-    if empty is None:
+    if find_empty is None:
         return True
     else:
-        row, col = empty
+        row, col = find_empty
 
     for val in range(1, len(board) + 1):
         if valid(board, val, (row, col)):
@@ -49,8 +49,3 @@ def solve(board):
             board[row][col] = 0
 
     return False
-
-
-
-
-   
